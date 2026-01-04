5860a0f31c85304afcecdd6f05e25132e209d759
okanruz <okanruz@gmail.com>
2026-01-04 17:38:44 +0300

    shop sayfasi eklendi

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/shop")
+def shop():
+    products = [
+        ("Kulaklık", 500),
+        ("Klavye", 900),
+        ("Mouse", 400)
+    ]
+    return render_template("shop.html", products=products)
