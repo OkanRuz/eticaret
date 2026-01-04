0b6444d0b33c90b499a5549c5f5a239de0ed8c4d
okanruz <okanruz@gmail.com>
2026-01-04 16:47:06 +0300

    Render port bind fix

--- eticaret/app.py
+++ eticaret/app.py
@@ -35,5 +35,6 @@ def admin():
     return render_template("admin.html")
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
